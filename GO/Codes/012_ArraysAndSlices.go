f21e66c203a463452162be724d447531e540ef22
harshit-273 <kalavadiaharshit@gmail.com>
2022-05-02 18:55:46 +0530

    added 2D array/slices

--- GO/Codes/012_ArraysAndSlices.go
+++ GO/Codes/012_ArraysAndSlices.go
@@ -19,19 +19,52 @@ func main() {
 	*/
 
 	////////////////////				Slices				////////////////////
-	var sliceArr [5]int = [5]int{0, 1, 2, 3, 4}
+	/*
+		var sliceArr [5]int = [5]int{0, 1, 2, 3, 4}
 
-	var sliFullCapacity []int = sliceArr[:] // it would give the full array to this slice
-	fmt.Println(sliFullCapacity)
+		var sliFullCapacity []int = sliceArr[:] // it would give the full array to this slice
+		fmt.Println(sliFullCapacity)
 
-	var sli []int = sliceArr[1:4] // this would give sli = {1,2,3} as we started from index 1 and we would end before the index given after ":"
-	fmt.Println(sli)
-	fmt.Println(cap(sli), len(sli)) // cap function would give how much this slice can store, although the length maybe smaller than or equal to capacity
+		var sli []int = sliceArr[1:4] // this would give sli = {1,2,3} as we started from index 1 and we would end before the index given after ":"
+		fmt.Println(sli)
+		fmt.Println(cap(sli), len(sli)) // cap function would give how much this slice can store, although the length maybe smaller than or equal to capacity
 
-	otherSli := make([]int, 0) // other way to create a slice without the array
-	fmt.Println(otherSli)
+		otherSli := make([]int, 0) // other way to create a slice without the array
+		fmt.Println(otherSli)
 
-	otherSli = append(otherSli, 1) // this would append the element "1" at the end of the slice
-	fmt.Println(otherSli)
+		otherSli = append(otherSli, 1) // this would append the element "1" at the end of the slice
+		fmt.Println(otherSli)
+	*/
 
+	////////////////////				Two Dimensional Slices 				////////////////////
+
+	sliOfSlices := make([][]int, 0) // make a 2D slice of length 4
+
+	for sliIndex := range sliOfSlices {
+		sliOfSlices[sliIndex] = make([]int, 2) // making each slice of length 2
+	}
+
+	fmt.Println(sliOfSlices) // printing the initial slice of slices
+
+	fmt.Println("--------")
+
+	sliOfSlices = append(sliOfSlices, []int{5, 6, 7}) // appending the slice in slice of slices
+	sliOfSlices = append(sliOfSlices, []int{1, 2})
+	sliOfSlices = append(sliOfSlices, []int{3, 4, 8, 9})
+
+	for outerIndex, sli := range sliOfSlices { // printing the slice of slices with indices and values
+		fmt.Print("outer index ")
+		fmt.Print(outerIndex)
+		fmt.Println(": ")
+		for innerIndex, value := range sli {
+			fmt.Print("inner index: ")
+			fmt.Print(innerIndex)
+			fmt.Print(" -> value:")
+			fmt.Print(value)
+			fmt.Print(", ")
+		}
+		fmt.Println()
+	}
+
+	fmt.Println(sliOfSlices) // printing the modified slice of slices
 }
