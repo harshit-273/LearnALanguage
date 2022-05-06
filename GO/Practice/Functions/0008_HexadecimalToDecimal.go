eaf62b71a1a55c2a52d98661177f43266a245899
harshit-273 <kalavadiaharshit@gmail.com>
2022-05-07 01:36:36 +0530

    incomplete hexadecimal to decimal, as need to learn strings

---
+++ GO/Practice/Functions/0008_HexadecimalToDecimal.go
@@ -0,0 +1,13 @@
+package main
+
+/*
+	- Here we will be creating a function which takes hexadecimal as input parameter and return decimal of it
+*/
+
+func hexadecimalToDecimal(hexNum string) int {
+	return 0 // Need to learn string in more better way, will add that Codes and Notes
+}
+
+func main() {
+
+}
