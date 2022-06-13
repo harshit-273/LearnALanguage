1fc5ab2d542f1af6376a902e47be96b8938a2854
harshit-273 <kalavadiaharshit@gmail.com>
2022-06-13 12:43:08 +0530

    find min and max added

---
+++ GO/Practice/ArraysAndSlices/0001_FindMinMax.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func FindMinMax(arr []int) (min int, max int) {
+	min = 9999
+	max = 0
+	for _, value := range arr {
+		if min > value {
+			min = value
+		}
+		if max < value {
+			max = value
+		}
+	}
+	return
+}
+
+func main() {
+	fmt.Println(FindMinMax([]int{45, 32, 54, 87, 21, 34}))
+}
