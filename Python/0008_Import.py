834d919e45209159dfc33d33cf30746254050abc
harshit-273 <kalavadiaharshit@gmail.com>
2022-07-25 22:12:22 +0530

    import added

---
+++ Python/0008_Import.py
@@ -0,0 +1,14 @@
+'''
+----------------------------------------------- Import -----------------------------------------------
+
+- everything is not in-built, sometimes we would want to immport something and there comes the use of import.
+
+Syntax:
+
+    from some_module import specific_attribute
+
+'''
+
+from math import pi
+
+print(pi)
