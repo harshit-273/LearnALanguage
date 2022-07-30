7fffaecb909e5171e4273f22bb0ee13776d22c2d
harshit-273 <kalavadiaharshit@gmail.com>
2022-07-30 23:59:13 +0530

    order of n pairsum added

--- Practice/0033_PairSum.go
+++ Practice/0033_PairSum.go
@@ -20,6 +20,23 @@ func PairSum(arr []int, k int) bool {
 	return false
 }
 
+func PairSum_On(arr []int, k int) bool {
+	low := 0
+	high := len(arr) - 1
+	for low != high {
+		sum := arr[low] + arr[high]
+		if sum == k {
+			return true
+		} else if sum < k {
+			low++
+		} else {
+			high--
+		}
+	}
+	return false
+}
+
 func main() {
-	fmt.Printf("If any pair was found having sum %d - %t", 31, PairSum([]int{2, 4, 7, 11, 14, 16, 20, 21}, 31))
+	fmt.Printf("If any pair was found having sum %d - %t\n", 31, PairSum([]int{2, 4, 7, 11, 14, 16, 20, 21}, 31))
+	fmt.Printf("If any pair was found having sum %d - %t", 31, PairSum_On([]int{2, 4, 7, 11, 14, 16, 20, 21}, 31))
 }
